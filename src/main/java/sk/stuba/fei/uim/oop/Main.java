8b7681dc84cab20938596866f071de85bef01ab5
xrideky <xrideky@stuba.sk>
2022-03-27 12:54:46 +0200

    zaklady enum

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,23 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
+        SvetoveStrany strana = SvetoveStrany.SEVER;
 
+        for(SvetoveStrany s : SvetoveStrany.values()){
+            checkSever(s);
+        }
+
+        System.out.println("---------------");
+        System.out.println(SvetoveStrany.valueOf("SEVER"));
+        checkSever(strana);
+        System.out.println(SvetoveStrany.valueOf("sever"));
+    }
+
+    public static void checkSever(SvetoveStrany s){
+        if(s.equals(SvetoveStrany.SEVER)){
+            System.out.println("je to Sever");
+        } else{
+            System.out.println("nie je to Sever");
+        }
     }
 }
