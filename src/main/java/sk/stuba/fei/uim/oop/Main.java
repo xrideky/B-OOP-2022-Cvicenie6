2ed390d40c97b4335b6f3057bc55cc0b8d012ea4
xrideky <xrideky@stuba.sk>
2022-03-27 14:08:32 +0200

    panel v paneli, farby

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -9,15 +9,32 @@ public class Main {
         JFrame okno = new JFrame();
         okno.setVisible(true);
         okno.setSize(300,300);
-        okno.setResizable(false);
+        okno.setResizable(true);
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         okno.setLayout(new GridLayout(3,4));  //GRID LAYOUT
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,2));
+        panel.setBackground(Color.CYAN);
+        okno.getContentPane().setBackground(new Color(190,180,50));
+
+        for(int i=0;i<4;i++){
+            panel.add(new JLabel("Panel " + i));
+        }
+
 
         JLabel label = new JLabel("Hello world");
         okno.add(label);
 
+
         for(int i=0;i<10;i++){
-            okno.add(new JLabel("Hello " + i));
+            if(i==8){
+                okno.add(panel);
+
+            } else{
+                okno.add(new JLabel("Hello " + i));
+            }
+
         }
 
 
